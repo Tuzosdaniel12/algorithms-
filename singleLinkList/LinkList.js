5ee5adea2529307c713a9a1acae0bb6a8f717db0
daniel <danielsoledad@gmail.com>
2021-10-20 02:54:27 -0700

    Adding SinglyLinkedList Class definition

--- singleLinkList/LinkList.js
+++ singleLinkList/LinkList.js
@@ -6,3 +6,11 @@ class Node{
         this.next = null;
     }
 }
+
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+}
