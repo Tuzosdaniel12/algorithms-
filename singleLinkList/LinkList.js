5dfa07ded95dd7bc3f62c7a06025eabe0f5853ad
daniel <danielsoledad@gmail.com>
2021-10-20 03:39:52 -0700

    Adding pop and shift to list structure

--- singleLinkList/LinkList.js
+++ singleLinkList/LinkList.js
@@ -29,7 +29,32 @@ class SinglyLinkedList{
         return this;
     }
     pop(){
-        
+        if(!this.head ) return undefined;
+        let current = this.head;
+        let newTail = this.head;
+        while(current.next){
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if(this.length <= 0){
+            this.head = null;
+			this.tail = null;
+        }
+        return current;
+    }
+    shift(){
+        if (!this.head) return undefined;
+        let head = this.head
+        this.head = head.next
+        this.length--;
+        if (this.length <= 0) {
+			this.head = null;
+			this.tail = null;
+		}
+        return head;
     }
 }
 
@@ -37,5 +62,16 @@ const list = new SinglyLinkedList();
 
 list.push(1)
 list.push(2);
+list.push(3);
+list.push(4);
 
+// console.log(list.pop())
+// console.log(list.pop());
+// console.log(list.pop());
+
+
+console.log(list.shift());
+console.log(list.shift());
+console.log(list.shift());
+console.log(list.shift());
 console.log(list)
